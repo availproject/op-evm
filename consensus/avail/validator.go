fb3e2bf4577476a684f990ed6d06d6d9bd77d8d7
Tuomas Mäkinen <tuomas.makinen@equilibrium.co>
2022-09-28 15:06:40 +0300

    Refactor the core logic to use new validator
    This change switches out from old validator logic written _in_ `avail`
    consensus and uses the new functionality from sub-package.
    
    This also disables the P2P syncing for validator (and watchtower) and
    purely works on data received from Avail (leveraging the new
    `avail.Stream`).
    
    This is first change towards new functionality and lacks the support for
    fraudproof handling.

--- consensus/avail/validator.go
+++ consensus/avail/validator.go
@@ -1,121 +1,54 @@
 package avail
 
 import (
-	"bytes"
-	"fmt"
-	"log"
-
-	"github.com/0xPolygon/polygon-edge/blockchain"
 	"github.com/0xPolygon/polygon-edge/types"
+	"github.com/maticnetwork/avail-settlement/consensus/avail/validator"
 	"github.com/maticnetwork/avail-settlement/pkg/avail"
 	"github.com/maticnetwork/avail-settlement/pkg/block"
+
+	avail_types "github.com/centrifuge/go-substrate-rpc-client/v4/types"
 )
 
 type ValidatorSet []types.Address
 
-type dataHandler struct {
-	blockchain *blockchain.Blockchain
-}
-
-func (dh *dataHandler) HandleData(bs []byte) error {
-	log.Printf("block handler: received batch w/ %d bytes\n", len(bs))
-
-	blk := types.Block{}
-	if err := blk.UnmarshalRLP(bs); err != nil {
-		return err
-	}
-
-	extraData := blk.Header.ExtraData
-	if len(extraData) > 0 && bytes.Contains(extraData, block.FraudproofPrefix) {
-		log.Printf("**************** FRAUD PROOF FOUND ************************")
-		addr := bytes.TrimPrefix(extraData, block.FraudproofPrefix)
-		if len(addr) < types.HashLength*2 {
-			return fmt.Errorf("invalid fraud proof block: %d/%q - target block hash invalid", blk.Number(), blk.Hash())
-		}
-
-		var hash types.Hash
-		err := hash.Scan(addr[:types.HashLength*2])
-		if err != nil {
-			return fmt.Errorf("invalid fraud proof block: %d/%q - cannot parse target block hash: %s", blk.Number(), blk.Hash(), err)
-		}
-
-		// TODO(tuommaki): Process fraud proof.
-		return nil
-	}
-
-	if err := dh.blockchain.VerifyFinalizedBlock(&blk); err != nil {
-		return fmt.Errorf("unable to verify block, %w", err)
-	}
-
-	if err := dh.blockchain.WriteBlock(&blk, "not-sure-what-source-yet-is"); err != nil {
-		return fmt.Errorf("failed to write block while bulk syncing: %w", err)
-	}
-
-	log.Printf("Received block header: %+v \n", blk.Header)
-	log.Printf("Received block transactions: %+v \n", blk.Transactions)
-
-	return nil
-}
-func (dh *dataHandler) HandleError(err error) {
-	log.Printf("block handler: error %#v\n", err)
-}
-
 func (d *Avail) runValidator() {
-	d.logger.Info("validator started")
+	availBlockStream := avail.NewBlockStream(d.availClient, d.logger, avail.BridgeAppID, 1)
+	validator := validator.New(d.blockchain, d.executor, types.StringToAddress(SequencerAddress))
 
-	// consensus always starts in SyncState mode in case it needs
-	// to sync with Avail and/or other nodes.
-	d.setState(SyncState)
-
-	handler := &dataHandler{blockchain: d.blockchain}
-
-	watcher, err := avail.NewBlockDataWatcher(d.availClient, avail.BridgeAppID, handler)
+	callIdx, err := avail.FindCallIndex(d.availClient)
 	if err != nil {
-		panic("couldn't create new avail block watcher: " + err.Error())
+		panic(err)
 	}
 
-	if err := watcher.Start(); err != nil {
-		panic("watcher start failed: " + err.Error())
-	}
-
-	defer watcher.Stop()
+	d.logger.Info("validator started")
 
 	// TODO: Figure out where do we need state cycle and how to implement it.
 	// Current version only starts the cycles for the future, doing nothing with it.
 	for {
+		var avail_blk *avail_types.SignedBlock
+
 		select {
 		case <-d.closeCh:
+			availBlockStream.Close()
 			return
-		default: // Default is here because we would block until we receive something in the closeCh
+		case avail_blk = <-availBlockStream.Chan():
 		}
 
-		// Start the state machine loop
-		d.runValidatorCycle()
-	}
-}
+		blk, err := block.FromAvail(avail_blk, avail.BridgeAppID, callIdx)
+		if err != nil {
+			d.logger.Error("cannot extract Edge block from Avail block %d: %s", avail_blk.Block.Header.Number, err)
+			continue
+		}
 
-func (d *Avail) runValidatorCycle() {
-	// Based on the current state, execute the corresponding section
-	switch d.getState() {
-	case AcceptState:
-		d.runAcceptState()
+		err = validator.Check(blk)
+		if err != nil {
+			d.logger.Error("invalid block %d/%q : %s", blk.Header.Number, blk.Header.Hash, err)
+			continue
+		}
 
-	case ValidateState:
-		d.runValidateState()
-
-	case SyncState:
-		d.runSyncState()
-	}
-}
-
-func (d *Avail) runValidateState() {
-	if !d.isState(ValidateState) {
-		return
-	}
-}
-
-func (d *Avail) runAcceptState() {
-	if !d.isState(AcceptState) {
-		return
+		err = validator.Apply(blk)
+		if err != nil {
+			d.logger.Error("cannot apply block %d/%q to blockchain: %s", blk.Header.Number, blk.Header.Hash, err)
+		}
 	}
 }

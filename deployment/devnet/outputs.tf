4168da28c6d90174c944c6d76c4d5c8ea0d1e2bb
daniel.vladco <vladcodaniel@gmail.com>
2023-04-26 19:16:21 +0300

    feat: attach aip/ebs to instances

--- deployment/devnet/outputs.tf
+++ deployment/devnet/outputs.tf
@@ -1,6 +1,11 @@
 output "all_instances" {
   value = local.all_instances
 }
+
+output "all_eips" {
+  value = concat([aws_eip.avail], [aws_eip.bootnode], aws_eip.node, aws_eip.watchtower)
+}
+
 output "ssh_pk" {
   value     = tls_private_key.pk.private_key_pem
   sensitive = true

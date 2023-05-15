ea906161636cd4036d46b002faba97f25f5a3c94
daniel.vladco <vladcodaniel@gmail.com>
2023-05-15 16:35:24 +0300

    feat: move security groups to security module

--- deployment/devnet/securitygroups.tf
+++ deployment/devnet/securitygroups.tf
@@ -1,9 +1,6 @@
 # Default Security Group of VPC
 resource "aws_default_security_group" "default" {
-  vpc_id     = aws_vpc.devnet.id
-  depends_on = [
-    aws_vpc.devnet
-  ]
+  vpc_id = module.networking.vpc_id
 
   ingress {
     from_port = "0"
@@ -20,156 +17,80 @@ resource "aws_default_security_group" "default" {
   }
 }
 
-# Watchtower
-
-resource "aws_security_group" "watchtower" {
-  count       = length(aws_instance.watchtower)
-  name        = format("allow-p2p-watchtower-%s-%02d", var.deployment_name, count.index + 1)
-  description = "Allow all p2p, grpc, jsonrpc traffic"
-  vpc_id      = aws_vpc.devnet.id
-  lifecycle {
-    create_before_destroy = true
-  }
+resource "aws_security_group" "allow_inbound_sg" {
+  name        = "allow-inbound-${var.deployment_name}"
+  description = "Allow grpc and jsonrpc traffic"
+  vpc_id      = module.networking.vpc_id
 }
-resource "aws_security_group_rule" "watchtower_grpc" {
-  count             = length(aws_instance.watchtower)
+
+resource "aws_security_group_rule" "allow_inbound_grpc_sgr" {
   type              = "ingress"
   from_port         = var.grpc_port
   to_port           = var.grpc_port
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = element(aws_security_group.watchtower, count.index).id
+  security_group_id = aws_security_group.allow_inbound_sg.id
 }
-resource "aws_security_group_rule" "watchtower_jsonrpc" {
-  count             = length(aws_instance.watchtower)
+
+resource "aws_security_group_rule" "allow_inbound_jsonrpc_sgr" {
   type              = "ingress"
   from_port         = var.jsonrpc_port
   to_port           = var.jsonrpc_port
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = element(aws_security_group.watchtower, count.index).id
-}
-resource "aws_security_group_rule" "watchtower_p2p" {
-  count             = length(aws_instance.watchtower)
-  type              = "ingress"
-  from_port         = element(aws_instance.watchtower, count.index).tags.P2PPort
-  to_port           = element(aws_instance.watchtower, count.index).tags.P2PPort
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = element(aws_security_group.watchtower, count.index).id
+  security_group_id = aws_security_group.allow_inbound_sg.id
 }
 
-resource "aws_network_interface_sg_attachment" "sg_watchtower_attachment_p2p" {
-  count                = length(aws_instance.watchtower)
-  security_group_id    = element(aws_security_group.watchtower, count.index).id
-  network_interface_id = element(aws_instance.watchtower, count.index).primary_network_interface_id
+resource "aws_security_group" "allow_inbound_p2p_sg" {
+  count       = length(local.all_nodes)
+  name        = "allow-inbound-p2p-${count.index + 1}-${var.deployment_name}"
+  description = "Allow p2p traffic"
+  vpc_id      = module.networking.vpc_id
 }
 
-# Boot node
-
-resource "aws_security_group" "bootnode" {
-  name        = format("allow-p2p-bootnode-%s", var.deployment_name)
-  description = "Allow all p2p, grpc, jsonrpc traffic"
-  vpc_id      = aws_vpc.devnet.id
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-resource "aws_security_group_rule" "bootnode_grpc" {
+resource "aws_security_group_rule" "allow_inbound_p2p_sgr" {
+  count             = length(local.all_nodes)
   type              = "ingress"
-  from_port         = var.grpc_port
-  to_port           = var.grpc_port
+  from_port         = local.all_nodes[count.index].p2p_port
+  to_port           = local.all_nodes[count.index].p2p_port
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.bootnode.id
-}
-resource "aws_security_group_rule" "bootnode_jsonrpc" {
-  type              = "ingress"
-  from_port         = var.jsonrpc_port
-  to_port           = var.jsonrpc_port
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.bootnode.id
-}
-resource "aws_security_group_rule" "bootnode_p2p" {
-  type              = "ingress"
-  from_port         = aws_instance.bootnode.tags.P2PPort
-  to_port           = aws_instance.bootnode.tags.P2PPort
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.bootnode.id
-}
-
-resource "aws_network_interface_sg_attachment" "sg_bootnode_attachment_p2p" {
-  security_group_id    = aws_security_group.bootnode.id
-  network_interface_id = aws_instance.bootnode.primary_network_interface_id
-}
-
-# Node
-
-resource "aws_security_group" "node" {
-  count       = length(aws_instance.node)
-  name        = format("allow-p2p-node-%s-%02d", var.deployment_name, count.index + 1)
-  description = "Allow all p2p, gpc, jsonrpc traffic"
-  vpc_id      = aws_vpc.devnet.id
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-resource "aws_security_group_rule" "node_grpc" {
-  count             = length(aws_instance.node)
-  type              = "ingress"
-  from_port         = var.grpc_port
-  to_port           = var.grpc_port
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = element(aws_security_group.node, count.index).id
-}
-resource "aws_security_group_rule" "node_jsonrpc" {
-  count             = length(aws_instance.node)
-  type              = "ingress"
-  from_port         = var.jsonrpc_port
-  to_port           = var.jsonrpc_port
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = element(aws_security_group.node, count.index).id
-}
-resource "aws_security_group_rule" "node_p2p" {
-  count             = length(aws_instance.node)
-  type              = "ingress"
-  from_port         = element(aws_instance.node, count.index).tags.P2PPort
-  to_port           = element(aws_instance.node, count.index).tags.P2PPort
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = element(aws_security_group.node, count.index).id
-}
-
-resource "aws_network_interface_sg_attachment" "node_attachment" {
-  count                = length(aws_instance.node)
-  security_group_id    = element(aws_security_group.node, count.index).id
-  network_interface_id = element(aws_instance.node, count.index).primary_network_interface_id
+  security_group_id = aws_security_group.allow_inbound_p2p_sg[count.index].id
 }
 
 # Outbound
 
-resource "aws_security_group" "allow_outbound_everywhere" {
+resource "aws_security_group" "allow_outbound_sg" {
   name        = "allow-everything-out-${var.deployment_name}"
   description = "Allow all outgoing traffic"
-  vpc_id      = aws_vpc.devnet.id
-  lifecycle {
-    create_before_destroy = true
-  }
+  vpc_id      = module.networking.vpc_id
 }
-resource "aws_security_group_rule" "allow_outbound_everywhere" {
+
+resource "aws_security_group_rule" "allow_outbound_all_sgr" {
   type              = "egress"
   from_port         = 0
   to_port           = 0
   protocol          = -1
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.allow_outbound_everywhere.id
+  security_group_id = aws_security_group.allow_outbound_sg.id
 }
-resource "aws_network_interface_sg_attachment" "allow_outbound_everywhere" {
-  count                = length(local.all_instances)
-  security_group_id    = aws_security_group.allow_outbound_everywhere.id
-  network_interface_id = element(local.all_instances, count.index).primary_network_interface_id
+
+# Attachments
+
+resource "aws_network_interface_sg_attachment" "inbound_node_sg_attach" {
+  count                = length(local.all_nodes)
+  security_group_id    = aws_security_group.allow_inbound_sg.id
+  network_interface_id = local.all_nodes[count.index].primary_network_interface_id
+}
+
+resource "aws_network_interface_sg_attachment" "outbound_node_sg_attach" {
+  count                = length(local.all_nodes)
+  security_group_id    = aws_security_group.allow_outbound_sg.id
+  network_interface_id = local.all_nodes[count.index].primary_network_interface_id
+}
+
+resource "aws_network_interface_sg_attachment" "inbound_node_p2p_sg_attach" {
+  count                = length(local.all_nodes)
+  security_group_id    = aws_security_group.allow_inbound_p2p_sg[count.index].id
+  network_interface_id = local.all_nodes[count.index].primary_network_interface_id
 }

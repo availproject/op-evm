629ca8270b704f487f945983be0955e0e1875a83
daniel.vladco <vladcodaniel@gmail.com>
2023-05-15 15:19:07 +0300

    feat: add iam profile to instances allowing ssm operations

--- deployment/devnet/modules/security/policies.tf
+++ deployment/devnet/modules/security/policies.tf
@@ -1,6 +1,16 @@
+resource "aws_iam_policy" "ssm_github_secrets_read" {
+  name   = "ssm_github_secrets_read-${var.deployment_name}"
+  policy = data.aws_iam_policy_document.ssm_github_secrets_read.json
+}
+
 resource "aws_iam_policy" "ssm_node_secrets_read" {
   name   = "ssm_node_secrets_read-${var.deployment_name}"
   policy = data.aws_iam_policy_document.ssm_node_secrets_read.json
+}
+
+resource "aws_iam_policy" "ssm_node_secrets_write" {
+  name   = "ssm_node_secrets_write-${var.deployment_name}"
+  policy = data.aws_iam_policy_document.ssm_node_secrets_write.json
 }
 
 resource "aws_iam_policy" "s3_read" {
@@ -11,4 +21,20 @@ resource "aws_iam_policy" "s3_read" {
 resource "aws_iam_policy" "s3_write" {
   name   = "s3_write-${var.deployment_name}"
   policy = data.aws_iam_policy_document.s3_write.json
+}
+
+resource "aws_iam_policy" "lambda_invoke" {
+  name   = "lambda_invoke-${var.deployment_name}"
+  policy = data.aws_iam_policy_document.lambda_invoke.json
+}
+
+resource "aws_iam_policy" "session_manager" {
+  name        = "ec2_metrics_${var.deployment_name}"
+  path        = "/"
+  description = "Policy to provide permission to EC2"
+  policy      = data.aws_iam_policy_document.session_manager.json
+  tags = {
+    Name        = "devnet_role"
+    Provisioner = data.aws_caller_identity.provisioner.account_id
+  }
 }
--- deployment/devnet/securitygroups.tf
+++ deployment/devnet/securitygroups.tf
@@ -24,7 +24,7 @@ resource "aws_default_security_group" "default" {
 
 resource "aws_security_group" "avail" {
   name        = "allow-avail-all-${var.deployment_name}"
-  description = "Allow all rpc, ws and ssh traffic"
+  description = "Allow all rpc, ws traffic"
   vpc_id      = aws_vpc.devnet.id
   lifecycle {
     create_before_destroy = true
@@ -46,14 +46,7 @@ resource "aws_security_group_rule" "allow_ws_avail" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.avail.id
 }
-resource "aws_security_group_rule" "allow_ssh_avail" {
-  type              = "ingress"
-  from_port         = "22"
-  to_port           = "22"
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.avail.id
-}
+
 resource "aws_network_interface_sg_attachment" "sg_avail_attachment_rpc" {
   security_group_id    = aws_security_group.avail.id
   network_interface_id = aws_instance.avail.primary_network_interface_id
@@ -64,7 +57,7 @@ resource "aws_network_interface_sg_attachment" "sg_avail_attachment_rpc" {
 resource "aws_security_group" "watchtower" {
   count       = length(aws_instance.watchtower)
   name        = format("allow-p2p-watchtower-%s-%02d", var.deployment_name, count.index + 1)
-  description = "Allow all p2p, grpc, jsonrpc and ssh traffic"
+  description = "Allow all p2p, grpc, jsonrpc traffic"
   vpc_id      = aws_vpc.devnet.id
   lifecycle {
     create_before_destroy = true
@@ -97,15 +90,7 @@ resource "aws_security_group_rule" "watchtower_p2p" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = element(aws_security_group.watchtower, count.index).id
 }
-resource "aws_security_group_rule" "watchtower_ssh" {
-  count             = length(aws_instance.watchtower)
-  type              = "ingress"
-  from_port         = "22"
-  to_port           = "22"
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = element(aws_security_group.watchtower, count.index).id
-}
+
 resource "aws_network_interface_sg_attachment" "sg_watchtower_attachment_p2p" {
   count                = length(aws_instance.watchtower)
   security_group_id    = element(aws_security_group.watchtower, count.index).id
@@ -116,7 +101,7 @@ resource "aws_network_interface_sg_attachment" "sg_watchtower_attachment_p2p" {
 
 resource "aws_security_group" "bootnode" {
   name        = format("allow-p2p-bootnode-%s", var.deployment_name)
-  description = "Allow all p2p, grpc, jsonrpc and ssh traffic"
+  description = "Allow all p2p, grpc, jsonrpc traffic"
   vpc_id      = aws_vpc.devnet.id
   lifecycle {
     create_before_destroy = true
@@ -146,14 +131,7 @@ resource "aws_security_group_rule" "bootnode_p2p" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.bootnode.id
 }
-resource "aws_security_group_rule" "bootnode_ssh" {
-  type              = "ingress"
-  from_port         = "22"
-  to_port           = "22"
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.bootnode.id
-}
+
 resource "aws_network_interface_sg_attachment" "sg_bootnode_attachment_p2p" {
   security_group_id    = aws_security_group.bootnode.id
   network_interface_id = aws_instance.bootnode.primary_network_interface_id
@@ -164,7 +142,7 @@ resource "aws_network_interface_sg_attachment" "sg_bootnode_attachment_p2p" {
 resource "aws_security_group" "node" {
   count       = length(aws_instance.node)
   name        = format("allow-p2p-node-%s-%02d", var.deployment_name, count.index + 1)
-  description = "Allow all p2p, gpc, jsonrpc and ssh traffic"
+  description = "Allow all p2p, gpc, jsonrpc traffic"
   vpc_id      = aws_vpc.devnet.id
   lifecycle {
     create_before_destroy = true
@@ -197,15 +175,7 @@ resource "aws_security_group_rule" "node_p2p" {
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = element(aws_security_group.node, count.index).id
 }
-resource "aws_security_group_rule" "node_ssh" {
-  count             = length(aws_instance.node)
-  type              = "ingress"
-  from_port         = "22"
-  to_port           = "22"
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = element(aws_security_group.node, count.index).id
-}
+
 resource "aws_network_interface_sg_attachment" "node_attachment" {
   count                = length(aws_instance.node)
   security_group_id    = element(aws_security_group.node, count.index).id

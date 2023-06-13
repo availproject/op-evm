c44e9f92a75f90add26f1c75659d147b22b6bab3
Nevio <nevio@subspace.com>
2023-06-13 12:58:00 +0200

    Removing unused builtin defs

--- server/builtin.go
+++ server/builtin.go
@@ -2,13 +2,11 @@ package server
 
 import (
 	"github.com/0xPolygon/polygon-edge/chain"
-	consensusPolyBFT "github.com/0xPolygon/polygon-edge/consensus/polybft"
 	"github.com/0xPolygon/polygon-edge/secrets"
 	"github.com/0xPolygon/polygon-edge/secrets/awsssm"
 	"github.com/0xPolygon/polygon-edge/secrets/gcpssm"
 	"github.com/0xPolygon/polygon-edge/secrets/hashicorpvault"
 	"github.com/0xPolygon/polygon-edge/secrets/local"
-	"github.com/0xPolygon/polygon-edge/server"
 	"github.com/0xPolygon/polygon-edge/state"
 )
 
@@ -21,12 +19,4 @@ var secretsManagerBackends = map[secrets.SecretsManagerType]secrets.SecretsManag
 	secrets.HashicorpVault: hashicorpvault.SecretsManagerFactory,
 	secrets.AWSSSM:         awsssm.SecretsManagerFactory,
 	secrets.GCPSSM:         gcpssm.SecretsManagerFactory,
-}
-
-var genesisCreationFactory = map[server.ConsensusType]server.GenesisFactoryHook{
-	server.PolyBFTConsensus: consensusPolyBFT.GenesisPostHookFactory,
-}
-
-var forkManagerFactory = map[server.ConsensusType]server.ForkManagerFactory{
-	server.PolyBFTConsensus: consensusPolyBFT.ForkManagerFactory,
 }

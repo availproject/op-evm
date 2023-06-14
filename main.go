a6893f290742e663e05e312f3c3324fe6ea2eb88
daniel.vladco <vladcodaniel@gmail.com>
2023-06-14 18:52:34 +0300

    feat: add devnet subcommand to start a devnet

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/maticnetwork/avail-settlement/cmd/availaccount"
+	"github.com/maticnetwork/avail-settlement/cmd/devnet"
 	"github.com/maticnetwork/avail-settlement/cmd/server"
 )
 
@@ -17,6 +18,7 @@ func main() {
 	cmd.AddCommand(
 		server.GetCommand(),
 		availaccount.GetCommand(),
+		devnet.GetCommand(),
 		secrets.GetCommand(),
 	)
 	if err := cmd.Execute(); err != nil {

50bdd12bb2155f438a7a82bd4e04e033d377e15b
Nevio <nevio@subspace.com>
2023-06-16 13:45:39 +0200

    Documentation for package/test

--- pkg/test/random.go
+++ pkg/test/random.go
@@ -8,12 +8,27 @@ import (
 )
 
 var (
-	// Seed used in functions that generate random data. Its value can be
-	// specified with -seed parameter. It defaults to time.Now().Unix().
+	// Seed is a global variable used in functions that generate random data.
+	// It's value can be specified via a command-line flag `-seed`.
+	// By default, it uses the current Unix time.
 	Seed = flag.Int64("seed", time.Now().Unix(), "random seed used in tests")
 )
 
-// RandomBytes generates requested number of random bytes.
+// RandomBytes generates a slice of random bytes of the specified size.
+// This function uses the global Seed variable for random number generation.
+// It's used to produce deterministic results when Seed is specified.
+//
+// t is a pointer to testing.T, which is the parallel testing interface.
+// size is the number of random bytes to generate.
+//
+// Returns a slice of random bytes.
+//
+// Example usage:
+//
+//	func TestRandomBytes(t *testing.T) {
+//		bytes := RandomBytes(t, 10)
+//		// bytes now holds a slice of 10 random bytes
+//	}
 func RandomBytes(t *testing.T, size int) []byte {
 	t.Helper()
 
--- pkg/test/txpool_hub.go
+++ pkg/test/txpool_hub.go
@@ -9,11 +9,17 @@ import (
 	"github.com/maticnetwork/avail-settlement/pkg/blockchain"
 )
 
+// txpoolHub is a struct that holds a state and a Blockchain instance.
+// This struct implements various methods that interact with the blockchain.
 type txpoolHub struct {
 	state state.State
 	*blockchain.Blockchain
 }
 
+// GetNonce returns the nonce of an account at a specific state root.
+// root is the hash of the state root.
+// addr is the address of the account.
+// Returns 0 if an error occurs when creating a new snapshot or getting the account.
 func (t *txpoolHub) GetNonce(root types.Hash, addr types.Address) uint64 {
 	// TODO: Use a function that returns only Account
 	snap, err := t.state.NewSnapshotAt(root)
@@ -29,6 +35,11 @@ func (t *txpoolHub) GetNonce(root types.Hash, addr types.Address) uint64 {
 	return account.Nonce
 }
 
+// GetBalance returns the balance of an account at a specific state root.
+// root is the hash of the state root.
+// addr is the address of the account.
+// Returns a big integer representing the account's balance.
+// An error is returned if it fails to get a snapshot for the state root or if the account does not exist.
 func (t *txpoolHub) GetBalance(root types.Hash, addr types.Address) (*big.Int, error) {
 	snap, err := t.state.NewSnapshotAt(root)
 	if err != nil {
@@ -43,14 +54,33 @@ func (t *txpoolHub) GetBalance(root types.Hash, addr types.Address) (*big.Int, e
 	return account.Balance, nil
 }
 
+// GetBlockByHash retrieves a block by its hash from the blockchain.
+// h is the hash of the block.
+// full is a boolean that determines whether to retrieve a full block or not.
+// Returns the block corresponding to the hash and a boolean indicating the success of the operation.
 func (t *txpoolHub) GetBlockByHash(h types.Hash, full bool) (*types.Block, bool) {
 	return t.Blockchain.GetBlockByHash(h, full)
 }
 
+// Header returns the header of the latest block from the blockchain.
+// Returns a pointer to the header of the latest block.
 func (t *txpoolHub) Header() *types.Header {
 	return t.Blockchain.Header()
 }
 
+// NewTxpoolHub creates a new txpoolHub instance with the provided state and blockchain.
+// s is the current state of the blockchain.
+// bc is the current blockchain instance.
+// Returns a pointer to the newly created txpoolHub instance.
+//
+// Example usage:
+//
+//	func TestNewTxpoolHub(t *testing.T) {
+//		state := ... // initialize state
+//		bc := ... // initialize blockchain
+//		hub := NewTxpoolHub(state, bc)
+//		// hub now holds a new txpoolHub instance
+//	}
 func NewTxpoolHub(s state.State, bc *blockchain.Blockchain) *txpoolHub {
 	return &txpoolHub{state: s, Blockchain: bc}
 }

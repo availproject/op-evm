9237374790524ec71ed5f817772bfc7cd184a2af
Nevio <0x19@users.noreply.github.com>
2023-06-16 14:42:41 +0200

    Documentation for server package (#186)
    Co-authored-by: Nevio <nevio@subspace.com>

--- server/builtin.go
+++ server/builtin.go
@@ -10,10 +10,14 @@ import (
 	"github.com/0xPolygon/polygon-edge/state"
 )
 
+// GenesisFactoryHook is a type definition for a function that takes a chain configuration
+// and an engine name, and returns a function that accepts a state transition object and returns an error.
+// It is typically used to modify the behavior of the blockchain's genesis block production based on specific engine.
 type GenesisFactoryHook func(config *chain.Chain, engineName string) func(*state.Transition) error
 
-// secretsManagerBackends defines the SecretManager factories for different
-// secret management solutions
+// secretsManagerBackends is a map that associates a SecretsManagerType with a SecretsManagerFactory function.
+// This allows for the creation of different types of secrets manager depending on the desired backend,
+// including local storage, Hashicorp Vault, AWS SSM, and GCP SSM.
 var secretsManagerBackends = map[secrets.SecretsManagerType]secrets.SecretsManagerFactory{
 	secrets.Local:          local.SecretsManagerFactory,
 	secrets.HashicorpVault: hashicorpvault.SecretsManagerFactory,

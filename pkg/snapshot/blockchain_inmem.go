22783f56a7376fd056e45a3304c17489741d6bb3
Nevio <0x19@users.noreply.github.com>
2023-06-20 12:32:01 +0200

    Documentation for pkg/snapshot (#191)
    Co-authored-by: Nevio <nevio@subspace.com>

--- pkg/snapshot/blockchain_inmem.go
+++ pkg/snapshot/blockchain_inmem.go
@@ -6,33 +6,34 @@ import (
 	"github.com/0xPolygon/polygon-edge/helper/hex"
 )
 
-// memoryKV is an in memory implementation of the kv storage
+// memoryKV is an in-memory implementation of the kv storage.
 type memoryKV struct {
 	db map[string][]byte
 }
 
+// Set sets the value of the given key in the in-memory key-value storage.
 func (m *memoryKV) Set(p []byte, v []byte) error {
 	m.db[hex.EncodeToHex(p)] = v
-
 	return nil
 }
 
+// Get retrieves the value associated with the given key from the in-memory key-value storage.
 func (m *memoryKV) Get(p []byte) ([]byte, bool, error) {
 	v, ok := m.db[hex.EncodeToHex(p)]
 	if !ok {
 		return nil, false, nil
 	}
-
 	return v, true, nil
 }
 
+// Close closes the in-memory key-value storage.
 func (m *memoryKV) Close() error {
 	return nil
 }
 
-// NewMemoryDBSnapshotStorage creates the new storage reference with inmem kv store and snapshot support.
+// NewMemoryDBSnapshotStorage creates a new storage reference with an in-memory key-value store and snapshot support.
 func NewMemoryDBSnapshotStorage() (BlockchainKVSnapshotter, error) {
-	db := &memoryKV{map[string][]byte{}}
+	db := &memoryKV{db: map[string][]byte{}}
 
 	kv := &blockchainKVStorage{
 		db:    db,

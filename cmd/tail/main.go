8696afab24249d52ebf4b0f54a5284b212ff5ea3
Tuomas Mäkinen <tuomas.makinen@equilibrium.co>
2023-06-21 12:40:41 +0300

    Improve block type recognizion & fetch parent on fork

--- cmd/tail/main.go
+++ cmd/tail/main.go
@@ -9,27 +9,31 @@ import (
 	"github.com/hashicorp/go-hclog"
 	"github.com/juju/ansiterm"
 	"github.com/spf13/cobra"
+	"github.com/umbracle/ethgo"
+	"github.com/umbracle/ethgo/jsonrpc"
 
 	"github.com/maticnetwork/avail-settlement/pkg/avail"
 	"github.com/maticnetwork/avail-settlement/pkg/block"
+	"github.com/maticnetwork/avail-settlement/pkg/staking"
 )
 
 func GetCommand() *cobra.Command {
-	var availAddr string
+	var availAddr, jsonrpcAddr string
 	var offset int64
 	cmd := &cobra.Command{
 		Use:   "tail",
 		Short: "Follow Avail SL blockstream from Avail",
 		Run: func(cmd *cobra.Command, args []string) {
-			Run(availAddr, offset)
+			Run(availAddr, jsonrpcAddr, offset)
 		},
 	}
 	cmd.Flags().StringVar(&availAddr, "avail-addr", "ws://127.0.0.1:9944/v1/json-rpc", "Avail JSON-RPC URL")
+	cmd.Flags().StringVar(&jsonrpcAddr, "jsonrpc-addr", "http://127.0.0.1:10002/v1/json-rpc", "Avail Settlement Layer JSON-RPC URL")
 	cmd.Flags().Int64Var(&offset, "offset", 1, "Block offset; defaults to first block")
 	return cmd
 }
 
-func Run(availAddr string, offset int64) {
+func Run(availAddr, jsonrpcAddr string, offset int64) {
 	availClient, err := avail.NewClient(availAddr, hclog.NewNullLogger())
 	if err != nil {
 		panic(err)
@@ -41,6 +45,11 @@ func Run(availAddr string, offset int64) {
 	}
 
 	callIdx, err := avail.FindCallIndex(availClient)
+	if err != nil {
+		panic(err)
+	}
+
+	jsonrpcClnt, err := jsonrpc.NewClient(jsonrpcAddr)
 	if err != nil {
 		panic(err)
 	}
@@ -60,7 +69,7 @@ func Run(availAddr string, offset int64) {
 				continue
 			}
 
-			printBlock(tw, b)
+			printBlock(tw, jsonrpcClnt, b)
 		}
 
 		tw.Flush()
@@ -75,14 +84,14 @@ func abs(x int64) int64 {
 	}
 }
 
-func printBlock(tw *ansiterm.TabWriter, blk *types.Block) {
+func printBlock(tw *ansiterm.TabWriter, jsonrpcClnt *jsonrpc.Client, blk *types.Block) {
 	switch {
 	case blk.Number() == 0:
 		printGenesis(tw, blk)
 	case isFraudProofBlock(blk):
 		printFraudProofBlock(tw, blk)
 	case isBeginDisputeResolutionBlock(blk):
-		printBeginDisputeResolutionBlock(tw, blk)
+		printBeginDisputeResolutionBlock(tw, jsonrpcClnt, blk)
 	case isEndDisputeResolutionBlock(blk):
 		printSlashBlock(tw, blk)
 	default:
@@ -103,15 +112,29 @@ func printDefaultBlock(tw *ansiterm.TabWriter, blk *types.Block) {
 }
 
 func printFraudProofBlock(tw *ansiterm.TabWriter, blk *types.Block) {
+	targetBlockHash, _ := block.GetExtraDataFraudProofTarget(blk.Header)
 	tw.SetForeground(ansiterm.BrightYellow)
-	fmt.Fprintf(tw, "%d\t%s\t%s\t%d\t%s\n", blk.Number(), blk.Hash().String(), blk.ParentHash().String(), len(blk.Transactions), "FRAUDPROOF")
+	fmt.Fprintf(tw, "%d\t%s\t%s\t%d\t%s\n", blk.Number(), blk.Hash().String(), blk.ParentHash().String(), len(blk.Transactions), fmt.Sprintf("FRAUDPROOF (TARGET BLOCK: %s)", targetBlockHash.String()))
 	tw.Reset()
 }
 
-func printBeginDisputeResolutionBlock(tw *ansiterm.TabWriter, blk *types.Block) {
-	// TODO: Check if block is forking or not; take it into account when selecting color.
-	tw.SetForeground(ansiterm.BrightBlue)
-	fmt.Fprintf(tw, "%d\t%s\t%s\t%d\t%s\n", blk.Number(), blk.Hash().String(), blk.ParentHash().String(), len(blk.Transactions), "BEGIN DISPUTE RESOLUTION")
+func printBeginDisputeResolutionBlock(tw *ansiterm.TabWriter, jsonrpcClnt *jsonrpc.Client, blk *types.Block) {
+	parent, err := jsonrpcClnt.Eth().GetBlockByHash(ethgo.Hash(blk.ParentHash()), true)
+	if err != nil {
+		panic(err)
+	}
+
+	isFork := parent.Number+1 != blk.Number()
+
+	if isFork {
+		tw.SetForeground(ansiterm.BrightGreen)
+		fmt.Fprintf(tw, "%d -> %d\t%s -> %s\t%s\n", parent.Number, blk.Number(), blk.ParentHash().String(), blk.Hash().String(), "FORK")
+		fmt.Fprintf(tw, "%d\t%s\t%s\t%d\t%s\n", blk.Number(), blk.Hash().String(), blk.ParentHash().String(), len(blk.Transactions), "BEGIN DISPUTE RESOLUTION")
+	} else {
+		tw.SetForeground(ansiterm.BrightBlue)
+		fmt.Fprintf(tw, "%d\t%s\t%s\t%d\t%s\n", blk.Number(), blk.Hash().String(), blk.ParentHash().String(), len(blk.Transactions), "BEGIN DISPUTE RESOLUTION")
+	}
+
 	tw.Reset()
 }
 
@@ -128,8 +151,16 @@ func isFraudProofBlock(blk *types.Block) bool {
 }
 
 func isBeginDisputeResolutionBlock(blk *types.Block) bool {
-	_, exists := block.GetExtraDataBeginDisputeResolutionTarget(blk.Header)
-	return exists
+	var isBeginDisputeResolutionBlock bool
+	for _, tx := range blk.Transactions {
+		if !isBeginDisputeResolutionBlock {
+			// Ignore returned error on purpose. It's mostly for debugging purposes and useless here.
+			isBeginDisputeResolutionBlock, _ = staking.IsBeginDisputeResolutionTx(tx)
+		} else {
+			break
+		}
+	}
+	return isBeginDisputeResolutionBlock
 }
 
 func isEndDisputeResolutionBlock(blk *types.Block) bool {

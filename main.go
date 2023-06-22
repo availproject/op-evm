7111df5057673e86a18d41415e66d0387918020e
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2023-06-22 06:53:12 +0300

    Avail SL `tail` utility (#207)
    Add `tail` command to follow avail-settlement block stream from Avail.
    This allows for improved visibility when developing, testing and
    operating avail-settlement node.

--- main.go
+++ main.go
@@ -9,10 +9,11 @@ import (
 	"github.com/maticnetwork/avail-settlement/cmd/availaccount"
 	"github.com/maticnetwork/avail-settlement/cmd/devnet"
 	"github.com/maticnetwork/avail-settlement/cmd/server"
+	"github.com/maticnetwork/avail-settlement/cmd/tail"
 )
 
 func main() {
-	var cmd = &cobra.Command{
+	cmd := &cobra.Command{
 		Short: "Avail settlement layer",
 	}
 	cmd.AddCommand(
@@ -20,6 +21,7 @@ func main() {
 		availaccount.GetCommand(),
 		devnet.GetCommand(),
 		secrets.GetCommand(),
+		tail.GetCommand(),
 	)
 	if err := cmd.Execute(); err != nil {
 		log.Fatal(err)
